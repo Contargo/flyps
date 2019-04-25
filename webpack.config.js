83c72ec3473097cad6380cdb595d7441afd1ff76
Stefan Wagner <wagner@synyx.de>
2019-04-26 00:29:27 +0200

    Provide an empty module and create dist bundle
    Users will be able to use flyps via:
    * ES2015 module loading, i.e. import
    * UMD module loading, i.e. require
    * a global flyps variable, i.e. `<script>` tag

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  devtool: "source-map",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "flyps.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "flyps",
+    libraryTarget: "umd"
+  }
+};
