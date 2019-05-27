8bb38c3f7c6da2b1e79c8b9a9e3145690c077193
Stefan Wagner <wagner@synyx.de>
2019-05-27 16:48:02 +0200

    Replace webpack with rollup
    * We just don't need this whole webpack-stuff

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: pkg.browser,
+        format: "umd",
+        name: "flyps",
+        sourcemap: true,
+      },
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [babel({ exclude: "node_modules/**" })],
+  },
+];
